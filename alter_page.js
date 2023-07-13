a5ba8d24a0a251ca33ae162062cff725ab291f33
jnarwell <jcmarwell@gmail.com>
2023-07-13 07:36:52 -0700

    fixed page-switching (thank you sarah!)

--- alter_page.js
+++ alter_page.js
@@ -1,8 +1,31 @@
 function to_about(){
+    
     document.getElementById("data_area").style.display = "none";
     document.getElementById("about_body").style.display = "grid";
     document.getElementById("skills").style.display = "none";
-    document.getElementById("experience").style.display = "none";
-
-    document.getElementsByClassName("container").style.gridTemplateRows = "0.5fr 2.6fr 0.6fr 0.3fr";
+    try{
+        document.getElementById("experience").style.display = "none";   
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+    document.getElementsByTagName("body")[0].style.gridTemplateColumns = "0.5fr 2.6fr 0.6fr 0.3fr";
+    document.getElementsByTagName("body")[0].style.gridTemplateAreas = '"return return . . "". about_body . side_bar"". . . side_bar"". links . ."';
 }
+function to_skills(){
+    
+    document.getElementById("data_area").style.display = "block";
+    document.getElementById("about_body").style.display = "none";
+    document.getElementById("skills").style.display = "grid";
+    try{
+        document.getElementById("experience").style.display = "none";   
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+    document.getElementsByTagName("body")[0].style.gridTemplateColumns = "0.5fr 1.7fr 1.5fr 0.3fr";
+    document.getElementsByTagName("body")[0].style.gridTemplateAreas = '"return return . ."". about_body data_area side_bar"". . . side_bar"". links . ."';
+}
+
